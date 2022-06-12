a7acaa77e1e59719ab7ee0ae36bd472c3117b8a1
ibenacho Elvis <ibenachoelvis49@gmail.com>
2022-06-12 16:14:10 +0100

    up

--- main.js
+++ main.js
@@ -8,3 +8,33 @@ menuBtn.addEventListener('click', ()=>{
     nav.classList.toggle('hidden');
     document.body.classList.toggle('no-scroll')
 })
+
+/*Form Validation Variables*/
+const form = document.getElementById('form');
+const subjectInput = document.getElementById('subject');
+const nameInput = document.getElementById('fullname')
+const telInput = document.getElementById('tel')
+const emailInput =document.getElementById('email')
+const messageInput = document.getElementById('message')
+const msg = document.getElementById('msg')
+
+/*Form Validation */
+form.addEventListener( 'submit', (e)=>{
+    e.preventDefault();
+    console.log('clicked')
+    formValidate();
+});
+
+function formValidate(){
+    if(emailInput.value === '' ){
+        msg.innerHTML = 'Please fill all details'
+        console.log('Please Fill all the details')
+    }else{
+        console.log('success')
+        msg.innerHTML =" "
+
+        
+    }
+}
+
+
